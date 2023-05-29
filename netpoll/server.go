d624bd3b1d9410ab9cdf79dde4965c6507b11645
xiongch <xiongch@flatincbr.com>
2023-05-29 15:08:47 +0800

    cancel extra code

--- netpoll/server.go
+++ netpoll/server.go
@@ -2,11 +2,8 @@ package netpoll
 
 import (
 	"bufio"
-	"errors"
 	"io"
 	"net"
-	"strconv"
-	"strings"
 	"sync"
 	"syscall"
 	"tinyredis/log"
@@ -111,29 +108,4 @@ func (s *Server) handler() {
 			}
 		}
 	}
-}
-
-func getIPPort(addr string) (ip [4]byte, port int, err error) {
-	strs := strings.Split(addr, ":")
-	if len(strs) != 2 {
-		err = errors.New("addr error")
-		return
-	}
-
-	if len(strs[0]) != 0 {
-		ips := strings.Split(strs[0], ".")
-		if len(ips) != 4 {
-			err = errors.New("addr error")
-			return
-		}
-		for i := range ips {
-			data, err := strconv.Atoi(ips[i])
-			if err != nil {
-				return ip, 0, err
-			}
-			ip[i] = byte(data)
-		}
-	}
-	port, err = strconv.Atoi(strs[1])
-	return
 }
